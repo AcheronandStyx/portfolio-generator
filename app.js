67ff5dcbfb1e46769129f1e43c33b3e4aaf95493
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-02-16 19:48:10 -0600

    ignore added

--- app.js
+++ app.js
@@ -4,14 +4,13 @@
  but not including the zero-based index we provide as the second argument. So, to return through the 
  last index in the array, we provide the length of the array as the second argument.
  */
+
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js'); // link the other js file
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-// extract the command line args
-const [argname, github] = profileDataArgs;
-
+/*
 const pageHTML = generatePage(argname, github);
 
 // write to output file index.html the html created by generatePage
@@ -19,6 +18,17 @@ fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+}); */
 
+
+// inquirer script
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
